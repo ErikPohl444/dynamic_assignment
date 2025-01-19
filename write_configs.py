0d5c9319c1787a28ce99ba28e882c9a775ace98e
Erik Pohl <epohl@dmigs.com>
2025-01-19 18:47:37 -0500

    attempted to handle removal of pycache file more gracefully

--- write_configs.py
+++ write_configs.py
@@ -10,25 +10,36 @@ class WriteConfigs:
             source_dict=None,
             file_name="configs.py"
     ):
+        # set up starter class attributes
         self.file_name = file_name
         self.primary_keys = primary_keys
         self.source_dict = source_dict
         self.missing_keys = []
+
+        # identify if any primary keys are missing from the source dictionary to get values from
         for pk in self.primary_keys:
             if pk not in self.source_dict.keys():
                 self.missing_keys.append(pk)
         if self.missing_keys:
             raise KeyError(f"Missing keys {','.join(self.missing_keys)}")
         else:
+            # write a shared state python script containing the variables and values
             with open(file_name, "wt") as cheat_handle:
                 for pk in self.primary_keys:
                     val = f"{pk}_xf = \"{self.source_dict[pk]}\"\n"
                     cheat_handle.write(val)
-        # remove pycache
+
+        # find pycache
         path = os.path.join(sys.path[0], "__pycache__")
-        pycache_file_name = [x for x in os.listdir(path) if file_name.split('.')[0] in x][0]
-        file_path = os.path.join(path, pycache_file_name)
         try:
-            os.remove(file_path)
-        except FileNotFoundError:
+            pycache_file_name = [x for x in os.listdir(path) if file_name.split('.')[0] in x][0]
+        except IndexError:
             pass
+        else:
+            file_path = os.path.join(path, pycache_file_name)
+
+            # remove pycache if it exists
+            try:
+                os.remove(file_path)
+            except FileNotFoundError:
+                pass
